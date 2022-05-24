2fa6a2b6d686014ded5983ad2cbe2eab43754793
Dhaesha M <dhaesha1@gmail.com>
2022-05-24 17:29:01 -0400

    move navbar from homepage to Function Component

--- nft-market-2/components/layout/BaseLayout.tsx
+++ nft-market-2/components/layout/BaseLayout.tsx
@@ -1,14 +1,18 @@
 import { FunctionComponent } from "react";
+import Navbar from "../navbar";
 
 const BaseLayout: FunctionComponent = ({ children }) => {
   return (
-    //   give page margin
-    <div className=" py-16 bg-gray-50 overflow-hidden min-h-screen">
-      {/* max div for screensize/viewport  */}
-      <div className="max-w-7xl mx-auto px-4 spacey-8 sm:px-6 lg:px-8">
-        {children}
+    <>
+      <Navbar />
+      {/* // give page margin */}
+      <div className=" py-16 bg-gray-50 overflow-hidden min-h-screen">
+        {/* max div for screensize/viewport  */}
+        <div className="max-w-7xl mx-auto px-4 spacey-8 sm:px-6 lg:px-8">
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
