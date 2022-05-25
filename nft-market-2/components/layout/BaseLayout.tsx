f5b453913970429a38a0d6b9269f08ae29cf9ee4
Dhaesha M <dhaesha1@gmail.com>
2022-05-25 10:21:24 -0400

    add sample json data from mypinyata that will give out nft characteristics + start homepage layout using tailwind

--- nft-market-2/components/layout/BaseLayout.tsx
+++ nft-market-2/components/layout/BaseLayout.tsx
@@ -5,10 +5,8 @@ const BaseLayout: FunctionComponent = ({ children }) => {
   return (
     <>
       <Navbar />
-      {/* // give page margin */}
-      <div className=" py-16 bg-gray-50 overflow-hidden min-h-screen">
-        {/* max div for screensize/viewport  */}
-        <div className="max-w-7xl mx-auto px-4 spacey-8 sm:px-6 lg:px-8">
+      <div className="py-16 bg-gray-50 overflow-hidden min-h-screen">
+        <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
           {children}
         </div>
       </div>
