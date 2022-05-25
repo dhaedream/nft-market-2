d76b5e47e8a752a095ee011e1def274ac014597c
Dhaesha M <dhaesha1@gmail.com>
2022-05-25 11:25:54 -0400

    add NftItem + NftList components

--- nft-market-2/components/index.ts
+++ nft-market-2/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Navbar } from "./navbar";
 export { default as BaseLayout } from "./layout/BaseLayout";
+export { default as Nftlist } from "./nft/list";
+export { default as NftItem } from "./nft/item";
