b8173321f584e888c23273df5df2da30e17c6611
Dhaesha M <dhaesha1@gmail.com>
2022-05-27 23:13:21 -0400

    able to follow path to nft create page

---
+++ nft-market-2/pages/nft/create.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import { BaseLayout } from "../../components";
+
+const NftCreate: NextPage = () => {
+  return <BaseLayout>NftCreate</BaseLayout>;
+};
+
+export default NftCreate;
