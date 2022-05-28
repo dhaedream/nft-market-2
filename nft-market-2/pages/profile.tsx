18bd6b8760cd7eb04182b651fb74f52783f1a187
Dhaesha M <dhaesha1@gmail.com>
2022-05-27 23:58:52 -0400

    add active link

--- nft-market-2/pages/profile.tsx
+++ nft-market-2/pages/profile.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import { BaseLayout } from "../components";
 
 const Profile: NextPage = () => {
-  return <div>Profile</div>;
+  return <BaseLayout>Profile</BaseLayout>;
 };
 
 export default Profile;
