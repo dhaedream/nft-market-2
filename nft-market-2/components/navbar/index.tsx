1d77849e7ead352163b73e31053c4759911cde36
Dhaesha M <dhaesha1@gmail.com>
2022-05-29 12:54:19 -0400

    add active link feature to navbar

--- nft-market-2/components/navbar/index.tsx
+++ nft-market-2/components/navbar/index.tsx
@@ -3,6 +3,7 @@ import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 import Link from "next/link";
+import ActiveLink from "../link";
 
 const navigation = [
   { name: "Marketplace", href: "/", current: true },
@@ -42,19 +43,19 @@ export default function Navbar() {
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <Link href={item.href} key={item.name}>
+                      <ActiveLink
+                        href={item.href}
+                        key={item.name}
+                        activeClass="bg-gray-900 text-white"
+                      >
                         <a
-                          className={classNames(
-                            item.current
-                              ? "bg-gray-900 text-white"
-                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                            "px-3 py-2 rounded-md text-sm font-medium"
-                          )}
+                          className="text-gray-300 hover:bg-gray-700 hover:text-white
+                            px-3 py-2 rounded-md text-sm font-medium"
                           aria-current={item.current ? "page" : undefined}
                         >
                           {item.name}
                         </a>
-                      </Link>
+                      </ActiveLink>
                     ))}
                   </div>
                 </div>
