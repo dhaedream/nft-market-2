a624deba24e47df5f101c37a49b58c91889fa665
Dhaesha M <dhaesha1@gmail.com>
2022-06-01 09:27:24 -0400

    set up a path to send web3 data to homepage

--- nft-market-2/components/providers/web3/index.tsx
+++ nft-market-2/components/providers/web3/index.tsx
@@ -1,17 +1,18 @@
 import { createContext, FunctionComponent, useState, useContext } from "react";
-import { MetaMaskInpageProvider } from "@metamask/providers";
-import { Contract, providers } from "ethers";
+import { createDefaultState, Web3State } from "./utils";
+// import { MetaMaskInpageProvider } from "@metamask/providers";
+// import { Contract, providers } from "ethers";
 
-export type Web3State = {
-  ethereum?: MetaMaskInpageProvider;
-  provider?: providers.Web3Provider;
-  contract?: Contract;
-};
+// export type Web3State = {
+//   ethereum?: MetaMaskInpageProvider;
+//   provider?: providers.Web3Provider;
+//   contract?: Contract;
+// };
 
-const Web3Context = createContext<Web3State>(null);
+const Web3Context = createContext<Web3State>(createDefaultState());
 
 const Web3Provider: FunctionComponent = ({ children }) => {
-  const [web3Api, setWeb3Api] = useState<Web3State>(null);
+  const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState());
   return (
     <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
   );
--- nft-market-2/pages/index.tsx
+++ nft-market-2/pages/index.tsx
@@ -8,9 +8,13 @@ import { NftMetaData } from "@_types/nft";
 import { useWeb3 } from "@providers/web3";
 
 const Home: NextPage = () => {
-  // const data = useWeb3();
+  const { ethereum, provider, isLoading, contract } = useWeb3();
   return (
     <BaseLayout>
+      {`ethereum: ${ethereum}, `}
+      {`provider: ${provider}, `}
+      {`is loading: ${isLoading}, `}
+      {`contract: ${contract}, `}
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
         <div className="absolute inset-0">
           <div className="bg-white h-1/3 sm:h-2/3" />
